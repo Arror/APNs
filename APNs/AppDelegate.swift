d6d590afd149d118b28e4b6eeefd661c5050c1ba
Arror <hallo.maqiang@gmail.com>
2019-03-03 11:17:33 +0800

    Init project

---
+++ APNs/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  APNs
+//
+//  Created by Arror on 2019/3/3.
+//  Copyright © 2019 Arror. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+}
+
---
+++ APNs/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  APNs
+//
+//  Created by Arror on 2019/3/3.
+//  Copyright © 2019 Arror. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}
+
