b74f42396312606af64030724788d5cd55d1fa5b
Arror <hallo.maqiang@gmail.com>
2019-03-15 23:10:25 +0800

    Add app

---
+++ flutter/apns/lib/app/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: Center(
+        child: Text('Hallo world.'),
+      ),
+    );
+  }
+}
