2ee2f5783ccc1644324128aee6b3c525c2a267ba
Arror <hallo.maqiang@gmail.com>
2019-03-30 17:25:33 +0800

    Update log

--- APNs/ViewController.swift
+++ APNs/ViewController.swift
@@ -51,38 +51,38 @@ class ViewController: NSViewController {
             let tokenPair = self.tokenTab.tokenPair
             guard
                 !tokenPair.1.isEmpty else {
-                    throw NSError.makeMessageError(message: "No Token")
+                    throw NSError.makeMessageError(message: "无设备Token")
             }
             guard
                 let payload = self.jsonTextView.string.data(using: .utf8) else {
-                    throw NSError.makeMessageError(message: "Payload error")
+                    throw NSError.makeMessageError(message: "负载格式错误")
             }
             guard
                 let cert = self.certTab.certificate else {
-                    throw NSError.makeMessageError(message: "Certificate info error")
+                    throw NSError.makeMessageError(message: "无法创建推送服务")
             }
             try AppUser.current.updateProvider(withCertificate: cert)
             guard
                 let provider = AppUser.current.provider else {
-                    throw NSError.makeMessageError(message: "Provider error")
+                    throw NSError.makeMessageError(message: "无法创建推送服务")
             }
             let server = tokenPair.0
             tokenPair.1.forEach { token in
                 let str: String = {
                     switch server {
                     case .sandbox:
-                        return "Sandbox"
+                        return "测试"
                     case .production:
-                        return "Productuon"
+                        return "生产"
                     }
                 }()
-                AppService.current.logger.log(level: .debug, message: "Send Notification, Server: \(str), Token: \(token)")
+                AppService.current.logger.log(level: .debug, message: "发送通知, 环境: \(str), Token: \(token)")
                 provider.send(server: server, options: .default, token: token, payload: payload) { resp in
                     switch resp {
                     case .success:
-                        AppService.current.logger.log(level: .debug, message: "Send Notification success")
+                        AppService.current.logger.log(level: .debug, message: "发送通知成功")
                     case .failure(let error):
-                        AppService.current.logger.log(level: .error, message: error.localizedDescription)
+                        AppService.current.logger.log(level: .error, message: "推送失败，原因：\(error.localizedDescription)")
                     }
                 }
             }
