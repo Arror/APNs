8ee685705463edec7d6db4f1e0e073eb67974584
Arror <hallo.maqiang@gmail.com>
2019-03-30 22:55:13 +0800

    Add ProviderPage

--- flutter/apns/lib/app/app.dart
+++ flutter/apns/lib/app/app.dart
@@ -4,12 +4,109 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       title: 'APNs Provider',
       home: Scaffold(
         appBar: AppBar(
           title: Text('APNs Provider'),
         ),
-        body: Scaffold()
+        body: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: ProviderPage(),
+            ),
+            Expanded(
+              flex: 2,
+              child: Scaffold(
+              
+              ),
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
+
+class ProviderPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+            ],
+          )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.edit),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return SimpleDialog(
+                contentPadding: EdgeInsets.only(top: 8.0, left: 24.0, right: 24.0, bottom: 12.0),
+                title: Text('编辑 Provider'),
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Certificate',
+                    ),
+                    onTap: () {
+                      print('Tapped.');
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Team ID'
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Key ID'
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Bundle ID'
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: RaisedButton(
+                            color: Colors.grey,
+                            child: Text('取消'),
+                            onPressed: () {
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: RaisedButton(
+                            child: Text('确定'),
+                            onPressed: () {
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              );
+            }
+          );
+        },
       ),
     );
   }
