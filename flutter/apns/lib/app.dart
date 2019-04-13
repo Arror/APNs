fb59fdf6afc985cd6af01854ed1658e794077d8f
Arror <hallo.maqiang@gmail.com>
2019-04-13 22:35:16 +0800

    Add home

--- flutter/apns/lib/app.dart
+++ flutter/apns/lib/app.dart
@@ -1,36 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:apns/plugins/system_action.dart';
+import 'package:apns/home.dart';
 
 class App extends StatelessWidget {
-
-  final _systemPlugin = APNsSystemPlugin();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'APNs',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('APNs Provider'),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.minimize),
-              onPressed: () {
-                _systemPlugin.performMiniaturize();
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.close),
-              onPressed: () {
-                _systemPlugin.performClose();
-              },
-            )
-          ],
-        ),
-        body: Container(),
-      ),
+      home: Home(),
     );
   }
 }
+
---
+++ flutter/apns/lib/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:apns/plugins/system_action.dart';
+
+class Home extends StatelessWidget {
+  
+  final _systemPlugin = APNsSystemPlugin();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('APNs Provider'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.minimize),
+            onPressed: () {
+              _systemPlugin.performMiniaturize();
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              _systemPlugin.performClose();
+            },
+          )
+        ],
+      ),
+      body: Container()
+    );
+  }
+}
