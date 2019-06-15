82ef024df1a283f49f725d3ddbaeec7f957c0db4
Arror <hallo.maqiang@gmail.com>
2019-06-15 22:01:50 +0800

    Remove `Flutter`

--- flutter/apns/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:apns/app.dart';
-
-void main() {
-  debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
-  runApp(App());
-}
-
